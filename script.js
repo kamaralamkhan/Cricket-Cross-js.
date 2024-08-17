2945d12002b193dd0d21cf7893ac4367285e8ce3
kamaralamkhan <kingk6491@gmail.com>
2024-08-17 21:08:52 +0530

    Calulator with raw JS

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentDisplay = '';
+
+function appendToDisplay(value) {
+    currentDisplay += value;
+    document.querySelector('#display').value = currentDisplay;
+}
+
+function clearDisplay() {
+    currentDisplay = '';
+    document.querySelector('#display').value = '';
+}
+
+function calculateResult() {
+    try {
+        currentDisplay = eval(currentDisplay);
+        document.querySelector('#display').value = currentDisplay;
+    } catch (e) {
+        document.querySelector('#display').value = 'Error';
+    }
+}
